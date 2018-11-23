18173865caf4aa9f92d7e68ef2bb13881f0858fb
Victor Domecq <vdomecq@ensc.fr>
2018-11-23 17:26:20 -0500

    First Commit

---
+++ Domain/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Player
+    {
+        private string name;
+
+        private int score;
+
+        public Player(string playerName)
+        {
+            this.name = playerName;
+            this.score = 0;
+        }
+
+        public void incrementScore()
+        {
+            this.score += 5;
+        }
+
+        public int getScore()
+        {
+            return this.score;
+        }
+    }
+}
--- LaserGame/Program.cs
+++ LaserGame/Program.cs
@@ -10,6 +10,8 @@ namespace LaserGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
         }
     }
 }
