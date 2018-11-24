db097a2bccf4ba98326fc926556ca9fced72e5e7
Victor Domecq <vdomecq@ensc.fr>
2018-11-23 19:18:22 -0500

    Add Position

---
+++ Domain/Position.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public struct Position
+    {
+        private readonly string name;
+
+        private readonly int bonus;
+
+        private readonly int malus;
+
+        public Position(string name, int bonus, int malus)
+        {
+            this.name = name;
+            this.bonus = bonus;
+            this.malus = malus;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public int Bonus
+        {
+            get { return this.bonus; }
+        }
+
+        public int Malus
+        {
+            get { return this.malus; }
+        }
+    }
+
+    static public class Positions
+    {
+        static private Position[] positions = new Position[]
+        {
+            new Position("Épaule", 15, 5),
+            new Position("Arme", 20, 8),
+            new Position("Torse", 10, 3),
+            new Position("Dos", 8, 2)
+        };
+
+        static public Position GetPositionByString(string positionName)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                if (positions[i].Name == positionName)
+                    return positions[i];
+            }
+
+            return new Position("", 0, 0);
+        }
+    }
+}
---
+++ UnitTestProject/PositionsTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PositionsTest
+    {
+        private bool CheckAttributesValues(Position position, string expectedName, int expectedBonus, int expectedMalus)
+        {
+            return expectedName == position.Name
+                && expectedBonus == position.Bonus
+                && expectedMalus == position.Malus;
+        }
+
+
+        [TestMethod]
+        public void TestGetPositionWithShoulderString()
+        {
+            Position position = Positions.GetPositionByString("Épaule");
+
+            Assert.IsTrue(this.CheckAttributesValues(position, "Épaule", 15, 5));
+        }
+
+        [TestMethod]
+        public void TestGetPositionWithWeaponString()
+        {
+            Position position = Positions.GetPositionByString("Arme");
+
+            Assert.IsTrue(this.CheckAttributesValues(position, "Arme", 20, 8));
+        }
+
+        [TestMethod]
+        public void TestGetPositionWithTorsoString()
+        {
+            Position position = Positions.GetPositionByString("Torse");
+
+            Assert.IsTrue(this.CheckAttributesValues(position, "Torse", 10, 3));
+        }
+
+        [TestMethod]
+        public void TestGetPositionWithBackString()
+        {
+            Position position = Positions.GetPositionByString("Dos");
+
+            Assert.IsTrue(this.CheckAttributesValues(position, "Dos", 8, 2));
+        }
+
+        [TestMethod]
+        public void TestGetPositionWithWrongString()
+        {
+            Position position = Positions.GetPositionByString("fzefezze");
+
+            Assert.IsTrue(this.CheckAttributesValues(position, "", 0, 0));
+        }
+    }
+}
