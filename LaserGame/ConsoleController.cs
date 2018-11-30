d8b81936fe44d4126b2038563b7441b27e4e7254
Victor Domecq <vdomecq@ensc.fr>
2018-11-30 13:34:04 -0500

    ConsoleController + Tests unitaires

--- LaserGame/ConsoleController.cs
+++ LaserGame/ConsoleController.cs
@@ -9,41 +9,115 @@ namespace LaserGame
 {
     public class ConsoleController
     {
-        private IScoreboards gameScoreboards;
+        private IScoreboards gameScoreboards; //TODO remplacer par interface
 
-        public ConsoleController(Scoreboards gameScoreboards)
+        public void SetGameScoreboards(IScoreboards scoreboardsImplementation)
         {
-            this.gameScoreboards = gameScoreboards;
+            this.gameScoreboards = scoreboardsImplementation;
         }
 
-        public string parseCommand(string actionCommand)
+        public string ExecuteCommand(string command)
         {
-            switch (actionCommand)
+            string[] args = command.Split(' ');
+
+            switch (args.Length)
             {
-                case "global":
-                   
-                    break;
-               // case ":
-                    
-               //     break;
-                default:
-                    Console.WriteLine("Default case");
-                    break;
+                case 1:
+                    return ParseCommandWith1Parameter(args);
+                case 2:
+                    return ParseCommandWith2Parameters(args);
+                case 3:
+                    return ParseCommandWith3Parameters(args);
+                case 5:
+                    return ParseCommandWith5Parameters(args);
+
             }
 
-            return  DisplayHelp();
+            throw new Exception();
         }
 
-        public string DisplayHelp()
+        private string ParseCommandWith1Parameter(string[] args)
+        {
+            if (args[0] == "exit")
+                return args[0];
+
+            if (args[0] == "help")
+                return Help();
+
+            if (args[0] == "global")
+                return gameScoreboards.GetGlobalScore();
+
+            throw new Exception();
+        }
+
+        private string ParseCommandWith2Parameters(string[] args)
+        {
+            if (args[0] == "new")
+                return ReadNewGameFile(args[1]);
+
+            if (args[0] == "score")
+                return gameScoreboards.GetScorePlayer(args[1]);
+
+            if (args[0] == "shootBy")
+                return gameScoreboards.GetShootByPlayers(args[1]);
+
+            if (args[0] == "shootAt")
+                return gameScoreboards.GetShootAtPlayers(args[1]);
+
+            if (args[0] == "allStat")
+                return gameScoreboards.GetAllPlayerStatistics(args[1]);
+
+            throw new Exception();
+        }
+
+        private string ParseCommandWith3Parameters(string[] args)
+        {
+            if (args[1] == "shootBy")
+                return gameScoreboards.GetShootByPlayer(args[0], args[2]);
+
+            if (args[1] == "shootAt")
+                return gameScoreboards.GetShootAtPlayer(args[0], args[2]);
+
+            throw new Exception();
+
+        }
+
+        private string ParseCommandWith5Parameters(string[] args)
+        {
+            if (args[1] == "shootBy" && args[3] == "At")
+                return gameScoreboards.GetShootByPlayerAtPosition(args[0], args[2], args[4]);
+
+            if (args[1] == "shootAt" && args[3] == "At")
+                return gameScoreboards.GetShootAtPlayerAtPosition(args[0], args[2], args[4]);
+
+            throw new Exception();
+        }
+            private string ReadNewGameFile(string fileName)
+        {
+
+            Scoreboards newScoreboards = GameInitializer.Initialize(fileName);
+            this.SetGameScoreboards(newScoreboards);
+
+            return "Votre nouvelle partie a bien été lue";
+        }
+
+        private string Help()
         {
             string help;
 
-            help = "Aide et commandes possibles :\n";
+            help = "Voici la liste des commandes possibles :\n";
+            help += "exit : Quitte l'application\n";
+            help += "help : Affiche l'aide\n";
             help += "global : Donne le score de tous les joueurs\n";
-            help += "<playerName> shootBy : Donne la liste des joueurs qui ont tiré sur <playerName> à chacune des positions\n";
-            help += "<playerName> shootAt : Donne la liste des joueurs sur qui <playerName> a tiré à chacune des positions\n";
-            help += "<playerName> score : Donne le score de <playerName>\n";
-            help += "<playerName> all : Donne toutes les informations de <playerName>\n";
+            help += "new <fileName> : Récupère un fichier de données d'une partie\n";
+            help += "score <playerName> : Donne le score de <playerName>\n";
+            help += "shootBy <playerName> : Donne la liste des joueurs qui ont tiré sur <playerName> à chacune des positions\n";
+            help += "shootAt <playerName> : Donne la liste des joueurs sur qui <playerName> a tiré à chacune des positions\n";
+            help += "allStat <playerName> : Donne toutes les informations de <playerName>\n";
+            help += "<playerName1> shootBy <playerName2> : Donne les positions ou <playerName1> s'est fait tiré dessus par <playerName2>\n";
+            help += "<playerName1> shootAt <playerName2> : Donne les positions ou <playerName1> a tiré sur <playerName2>\n";
+            help += "<playerName1> shootBy <playerName2> At <position> : Donne le nombre de fois ou <playerName1> s'est fait tiré dessus par <playerName2> à <position>\n";
+            help += "<playerName1> shootAt <playerName2> At <position> : Donne le nombre de fois ou <playerName1> a tiré sur <playerName2> à <position>\n";
 
             return help;
         }
---
+++ UnitTestProject/ConsoleControllerTest.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using LaserGame;
+using System.Reflection;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ConsoleControllerTest
+    {
+        ConsoleController consoleController;
+
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            consoleController = new ConsoleController();
+            consoleController.SetGameScoreboards(new ScoreboardsImplementationTest());
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithExitArg()
+        {
+            string result = consoleController.ExecuteCommand("exit");
+            Assert.AreEqual("exit", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithHelpArg()
+        {
+            string result = consoleController.ExecuteCommand("help");
+
+            var privateHelpMethod = typeof(ConsoleController).GetMethod("Help",
+                        BindingFlags.NonPublic | BindingFlags.Instance);
+
+            string expectedValue = (string)privateHelpMethod.Invoke(consoleController, new object[] { });
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithGlobalArg()
+        {
+            string result = consoleController.ExecuteCommand("global");
+
+            Assert.AreEqual("Executing GetGlobalScore", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithNewFileArgs()
+        {
+            string result = consoleController.ExecuteCommand("new Game1");
+
+            Assert.AreEqual("Votre nouvelle partie a bien été lue", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithScoreArgs()
+        {
+            string result = consoleController.ExecuteCommand("score Quentin");
+
+            Assert.AreEqual("Executing GetPlayerScore with arg : Quentin", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithShootByAnd2Args()
+        {
+            string result = consoleController.ExecuteCommand("shootBy Quentin");
+
+            Assert.AreEqual("Executing GetShootByPlayer with arg : Quentin", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithShootAtAnd2Args()
+        {
+            string result = consoleController.ExecuteCommand("shootAt Quentin");
+
+            Assert.AreEqual("Executing GetShootAtPlayer with arg : Quentin", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithAllStatArgs()
+        {
+            string result = consoleController.ExecuteCommand("allStat Quentin");
+
+            Assert.AreEqual("Executing GetAllPlayerStatistics with arg : Quentin", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithShootByAnd3Args()
+        {
+            string result = consoleController.ExecuteCommand("Quentin shootBy Clément");
+
+            Assert.AreEqual("Executing GetShootByPlayer with args : Quentin and Clément", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithShootAtAnd3Args()
+        {
+            string result = consoleController.ExecuteCommand("Quentin shootAt Clément");
+
+            Assert.AreEqual("Executing GetShootAtPlayer with args : Quentin and Clément", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithShootByAnd5Args()
+        {
+            string result = consoleController.ExecuteCommand("Quentin shootBy Clément At Dos");
+
+            Assert.AreEqual("Executing GetShootByPlayerAtPosition with args : Quentin and Clément and Dos", result);
+        }
+
+        [TestMethod]
+        public void TestParseCommandWithShootAtAnd5Args()
+        {
+            string result = consoleController.ExecuteCommand("Quentin shootAt Clément At Dos");
+
+            Assert.AreEqual("Executing GetShootAtPlayerAtPosition with args : Quentin and Clément and Dos", result);
+        }
+
+    }
+
+    public class ScoreboardsImplementationTest : IScoreboards
+    {
+        public string GetGlobalScore()
+        {
+            return "Executing GetGlobalScore";
+        }
+
+        public string GetScorePlayer(string playerName)
+        {
+            return "Executing GetPlayerScore with arg : " + playerName;
+        }
+
+        public string GetShootByPlayers(string playerName)
+        {
+            return "Executing GetShootByPlayer with arg : " + playerName;
+        }
+
+        public string GetShootAtPlayers(string playerName)
+        {
+            return "Executing GetShootAtPlayer with arg : " + playerName;
+        }
+
+        public string GetAllPlayerStatistics(string playerName)
+        {
+            return "Executing GetAllPlayerStatistics with arg : " + playerName;
+        }
+
+        public string GetShootByPlayer(string playerName, string shooterName)
+        {
+            return "Executing GetShootByPlayer with args : " + playerName + " and " + shooterName;
+        }
+
+        public string GetShootAtPlayer(string playerName, string targetName)
+        {
+            return "Executing GetShootAtPlayer with args : " + playerName + " and " + targetName;
+        }
+
+        public string GetShootByPlayerAtPosition(string playerName, string shooterName, string positionName)
+        {
+            return "Executing GetShootByPlayerAtPosition with args : " + playerName + " and " + shooterName + " and " + positionName;
+        }
+
+        public string GetShootAtPlayerAtPosition(string playerName, string targetName, string positionName)
+        {
+            return "Executing GetShootAtPlayerAtPosition with args : " + playerName + " and " + targetName + " and " + positionName;
+        }
+    }
+}
