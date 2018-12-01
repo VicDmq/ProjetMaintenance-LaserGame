8cd4f6cb9688ad07302724b0ec2fbd405eaf8ce5
Victor Domecq <vdomecq@ensc.fr>
2018-12-01 18:26:34 -0500

    ScoreboardsFormatter + Test

---
+++ StringFormatter/ScoreboardsFormatter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringFormatter
+{
+    public static class ScoreboardsFormatter
+    {
+        static private string nbSpacesBetweenColumns = "  ";
+
+        static public string FormatPositionsNamesToRawString(string [] positions)
+        {
+            string rawFormatted = "";
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                rawFormatted += positions[i] + nbSpacesBetweenColumns;
+            }
+
+            return rawFormatted;
+        }
+
+        static public string FormatIntArrayToRawString(string[] positions, int[] array)
+        {
+            string rawformatted = "";
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                int nbLettersOfPositionName = positions[i].Length;
+
+                rawformatted += GetCell(nbLettersOfPositionName, array[i].ToString());
+
+                rawformatted += nbSpacesBetweenColumns;
+            }
+
+            return rawformatted;
+        }
+
+        private static string GetCell(int cellLength, string cellContent)
+        {
+            int nbSpacesToAddAtBeginning = cellLength / 2;
+            nbSpacesToAddAtBeginning += cellLength % 2 == 0 ? -1 : 0;
+
+            int nbSpacesToAddAtEnding = cellLength - cellLength / 2 - cellContent.Length;
+            nbSpacesToAddAtEnding += cellLength % 2 == 0 ? 1 : 0;
+
+            string cell = GetStringWithNSpaces(nbSpacesToAddAtBeginning);
+            cell += cellContent;
+            cell += GetStringWithNSpaces(nbSpacesToAddAtEnding);
+
+            return cell;
+        }
+
+        private static string GetStringWithNSpaces(int nbSpaces)
+        {
+            string stringWithNSpaces = "";
+
+            for (int i = 0; i < nbSpaces; i++)
+                stringWithNSpaces += " ";
+
+            return stringWithNSpaces;
+        }
+    }
+}
---
+++ UnitTestProject/ScoreboardsFormatterTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringFormatter;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ScoreboardsFormatterTest
+    {
+        [TestMethod]
+        public void TestFormatPositionsNamesToRawString()
+        {
+            string[] positionsNames = new string[] { "Épaule", "Arme", "Torse", "Dos" };
+
+            string raw = ScoreboardsFormatter.FormatPositionsNamesToRawString(positionsNames);
+
+            Assert.AreEqual("Épaule  Arme  Torse  Dos  ", raw);
+        }
+
+        [TestMethod]
+        public void TestFormatIntArrayToRawString()
+        {
+            string[] positionsNames = new string[] { "Épaule", "Arme", "Torse", "Dos" };
+            int[] array = new int[] { 11, 5, 6, 15 };
+
+            string raw = ScoreboardsFormatter.FormatIntArrayToRawString(positionsNames, array);
+
+            Assert.AreEqual("  11     5      6     15  ", raw);
+        }
+
+        private string InvokeGetCell(int cellLength, string cellContent)
+        {
+            var privateAndStaticMethodToTest = typeof(ScoreboardsFormatter).GetMethod("GetCell",
+                        BindingFlags.NonPublic | BindingFlags.Static);
+
+            return (string)privateAndStaticMethodToTest.Invoke(null, new object[] { cellLength, cellContent });
+        }
+
+        [TestMethod]
+        public void TestDisplayCellWithImpairedSize()
+        {
+            string cell = this.InvokeGetCell(7, "*");
+
+            Assert.AreEqual("   *   ", cell);
+        }
+
+        [TestMethod]
+        public void TestDisplayCellWithImpairedSizeAndCellContentOfSizeEquals2()
+        {
+            string cell = this.InvokeGetCell(7, "**");
+
+            Assert.AreEqual("   **  ", cell);
+        }
+
+        [TestMethod]
+        public void TestDisplayCellWithPairedSize()
+        {
+            string cell = this.InvokeGetCell(6, "*");
+
+            Assert.AreEqual("  *   ", cell);
+        }
+
+        [TestMethod]
+        public void TestDisplayCellWithPairedSizeAndCellContentOfSizeEquals2()
+        {
+            string cell = this.InvokeGetCell(6, "**");
+
+            Assert.AreEqual("  **  ", cell);
+        }
+    }
+}
